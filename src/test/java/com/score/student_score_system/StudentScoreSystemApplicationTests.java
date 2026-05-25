9ef033ed5536d0159c6e3b8cb2581ab13f7b82ad
Jellyyuki <yuki.feng.rujin@qq.com>
2026-05-25 10:00:23 +0800

    提交项目

---
+++ src/test/java/com/score/student_score_system/StudentScoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.score.student_score_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentScoreSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
